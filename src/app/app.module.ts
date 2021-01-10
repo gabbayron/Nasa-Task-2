c4efa5cdd48b3f190f9e18d6a1a2729b78132034
gabbayron <gabbayron2307@gmail.com>
2021-01-10 16:29:25 +0200

    env

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,20 +56,17 @@ import { AgmDirectionModule } from 'agm-direction';
     MaterialModule,
     NgxAuthFirebaseUIModule.forRoot(environment.firebase,
       () => 'Nasa Task', {
-      enableFirestoreSync: true, // enable/disable autosync users with firestore
-      toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
-      toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
-      authGuardFallbackURL: '/loggedout', // url for unauthenticated users - to use in combination with canActivate feature on a route
-      authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
-      passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
-      passwordMinLength: 8, // Password length min/max in forms independently of each componenet min/max.
-      // Same as password but for the name
+      enableFirestoreSync: true,
+      toastMessageOnAuthSuccess: false,
+      toastMessageOnAuthError: false,
+      authGuardFallbackURL: '/loggedout',
+      authGuardLoggedInURL: '/loggedin',
+      passwordMaxLength: 60,
+      passwordMinLength: 8,
       nameMaxLength: 50,
       nameMinLength: 2,
-      // If set, sign-in/up form is not available until email has been verified.
-      // Plus protected routes are still protected even though user is connected.
       guardProtectedRoutesUntilEmailIsVerified: true,
-      enableEmailVerification: true, // default: true
+      enableEmailVerification: true,
     }),
     AgmCoreModule.forRoot({
       apiKey: environment.googleMapsApiKey
